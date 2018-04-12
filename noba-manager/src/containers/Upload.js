9791ad15a359441a7297c2f5947927774d1629ec
car <carmelogustavo@gmail.com>
2018-04-12 19:28:38 +0200

    feat: upload working with BE

--- noba-manager/src/containers/Upload.js
+++ noba-manager/src/containers/Upload.js
@@ -14,50 +14,66 @@ class Upload extends Component {
       height: '',
       width: '',
       mainImage: '',
-      collection: []
-    }
+      collection: [],
+    };
 
   }
-    onDrop = (acceptedFiles, rejectedFiles) => { 
-      this.setState({
-        mainImage: this.state.collection[0] ? this.state.collection[0].preview : acceptedFiles[0].preview,
-        collection: [...this.state.collection, ...acceptedFiles]
-      });
-    }
 
-    handleDelete = e => {
-      const currId = e.target.id
-      this.setState({
-        collection: this.state.collection.filter(image => {
-          return image.preview !== currId
-        })
-      }, () => {
-        if (this.state.mainImage === currId) {
-          this.setState({
-            mainImage: this.state.collection[0].preview
-          })
-        }
-      })
-      
-    }
+  onDrop = (acceptedFiles, rejectedFiles) => {
+    this.setState({
+      mainImage: this.state.collection[0] ? this.state.collection[0].preview : acceptedFiles[0].preview,
+      collection: [...this.state.collection, ...acceptedFiles],
+    });
+  };
 
-    handleChange = e => {
-      this.setState({
-        [e.target.name]: e.target.value,
-      })
-    }
+  handleDelete = e => {
+    const currId = e.target.id;
+    this.setState({
+      collection: this.state.collection.filter(image => image.preview !== currId),
+    }, () => {
+      if (this.state.mainImage === currId) {
+        this.setState({
+          mainImage: this.state.collection[0].preview,
+        });
+      }
+    });
 
-    selectMain = e => {
-      console.log(e.target);
-      this.setState({
-        mainImage: e.target.src
-      })      
-    }
+  };
 
-    render() {
-      const { state } = this;
-      return (
-        <div className="Upload">
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  selectMain = e => {
+    console.log(e.target);
+    this.setState({
+      mainImage: e.target.src,
+    });
+  };
+
+  sendserver = () => {
+    var files = this.state.collection;
+    var formData = new FormData();
+    formData.append('title', this.state.title);
+    formData.append('material', this.state.material);
+    formData.append('year', this.state.year);
+    formData.append('height', this.state.height);
+    formData.append('width', this.state.width);
+    files.forEach(file => {
+      this.state.mainImage === file.preview ? formData.append('main-image', file) : formData.append(file.preview, file);
+    });
+
+    request.post('http://localhost:3002/upload')
+      .send(formData)
+      .end();
+  };
+
+  render() {
+    const { state } = this;
+    return (
+      <div className="Upload">
         <p>Upload new Artwork</p>
         <form>
           <div className="text-image">
@@ -85,32 +101,30 @@ class Upload extends Component {
               </div>
             </div>
             <Dropzone
-              multiple={true}
-              accept="image/*"
-              onDrop={this.onDrop.bind(this)}>
+            multiple={true}
+            accept="image/*"
+            onDrop={this.onDrop.bind(this)}>
               <p>Drop an image or click to select a file to upload.</p>
             </Dropzone>
           </div>
-          <div>
-            <button onClick={this.handleClick}>Upload</button>          
-          </div>
         </form>
         <div className="previews-container">
-          {this.state.collection.map( image => {
-            return(
-              <div className="preview-list" key={image.preview} >
-                <img 
-                  src={image.preview}
-                  alt={image.name}
-                  onClick={this.selectMain}
-                  className={this.state.mainImage === image.preview ? 'previews-selected previews' : 'previews'} />
+          {this.state.collection.map(image => {
+          return (
+            <div className="preview-list" key={image.preview} >
+                <img
+                src={image.preview}
+                alt={image.name}
+                onClick={this.selectMain}
+                className={this.state.mainImage === image.preview ? 'previews-selected previews' : 'previews'} />
                 <span onClick={this.handleDelete} id={image.preview} role="img" alt=''>🗑️</span>
               </div>
-            )}
-          )}
+          );}
+        )}
         </div>
+        <button onClick={this.sendserver}>Upload</button>
       </div>
-    );
+  );
   }
 }
 
